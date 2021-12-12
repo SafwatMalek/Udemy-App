9b5d33fe90ca677645c78429c1022e5cadc3e7f1
safwatmalek <safwat.nassif.malek@gmail.com>
2021-12-12 21:02:37 +0200

    [add] course list item design. [add] navigation bar for home.

--- app/src/main/java/com/example/udemyapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/udemyapp/ui/MainActivity.kt
@@ -1,20 +1,15 @@
-package com.example.udemyapp.ui.home
+package com.example.udemyapp.ui
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.udemyapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    val homeViewModel: HomeViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        homeViewModel.getCoursesList()
-
     }
 }
---
+++ app/src/main/java/com/example/udemyapp/ui/home/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.example.udemyapp.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.udemyapp.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeFragment : Fragment() {
+    val homeViewModel: HomeViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    companion object
+}
